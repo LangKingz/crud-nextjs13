e892bd8b9e4006576d51a2bead72513489e314fa
Gilang <109453882+LangKingz@users.noreply.github.com>
2023-05-11 22:36:33 +0700

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import Link from 'next/link';
 import React from 'react'
+
 
 export default function page() {
   return (
-    <div>page</div>
+    <div className='py-10 px-10' style={{
+      // border: '1px solid black',
+      height: '100vh',
+      width: '80vw',
+      position: 'relative',
+      left: '10%',
+      marginTop: '10%',
+      justifyItems: 'center',
+    }}>
+      <div className="container justify-center">
+        <Link className='btn btn-primary' href='/products'>Klik Main CRUD</Link>
+      </div>
+    </div>
   )
 }
